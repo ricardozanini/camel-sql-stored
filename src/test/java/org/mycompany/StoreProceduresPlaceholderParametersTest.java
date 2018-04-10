b605b4f7b9a7be779da33b1f8100ff0589d78d4a
Ricardo Zanini <ricardozanini@gmail.com>
2018-04-10 14:11:29 -0300

    2.21.0 reproducer

---
+++ src/test/java/org/mycompany/StoreProceduresPlaceholderParametersTest.java
@@ -0,0 +1,29 @@
+package org.mycompany;
+
+import java.util.Collections;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class StoreProceduresPlaceholderParametersTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("spring/camel-context.xml");
+    }
+    
+    @Test
+    public void test() throws InterruptedException {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+        mock.expectedBodyReceived().body().isEqualTo(true);
+        
+        template.sendBody("direct:sp-test", Collections.singletonMap("number", 7));
+        
+        assertMockEndpointsSatisfied();
+    }
+
+}
